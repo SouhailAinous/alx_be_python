59131caf61698d1b819603cc4a875034a6ba7ce7
Souhail Ainous <souhailainouss55@gmail.com>
2025-07-20 18:48:46 +0100

    modify the py file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5 
+#OPERATIONS
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+#PRINT THE RESULTE
+print("Addition of", number1, "and", number2, "is", addition)
+print("Subtraction of", number1, "and", number2, "is", subtraction)
+print("Multiplication of", number1, "and", number2, "is", multiplication)
